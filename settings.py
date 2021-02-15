62b99d7a292ba2b44d32627af95cd22747971eab
Kevin Tran <me@kevintran.codes>
2021-02-15 01:44:44 -0800

    Remove Minio crap

--- settings.py
+++ settings.py
@@ -6,10 +6,4 @@ load_dotenv()
 
 slack_token = os.getenv("SLACK_TOKEN")
 
-minio_endpoint = os.getenv("MINIO_ENDPOINT")
-minio_access_key = os.getenv("MINIO_ACCESS_KEY")
-minio_secret_key = os.getenv("MINIO_SECRET_KEY")
-minio_bucket = os.getenv("MINIO_BUCKET", "slack")
-minio_secure = os.getenv("MINIO_SECURE", "true").lower() == "true"
-
 file_output_directory = os.getenv("FILE_OUTPUT_DIRECTORY", "")
